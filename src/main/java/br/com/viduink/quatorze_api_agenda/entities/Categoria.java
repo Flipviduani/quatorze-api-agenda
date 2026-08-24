34434488f10ff5403b8f9400c850ee5eca32e007
Flipviduani <flipviduani@hotmail.com>
2026-08-24 12:44:26 -0300

    Updated Entities.

--- src/main/java/br/com/viduink/quatorze_api_agenda/entities/Categoria.java
+++ src/main/java/br/com/viduink/quatorze_api_agenda/entities/Categoria.java
@@ -1,4 +1,23 @@
 package br.com.viduink.quatorze_api_agenda.entities;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.List;
+
+@Entity
+@Table(name = "categorias")
+@Data
 public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "nome", length = 50, nullable = false, unique = true)
+    private String nome;
+
+
+    private List<Tarefa> tarefas;
 }
--- src/main/java/br/com/viduink/quatorze_api_agenda/entities/Tarefa.java
+++ src/main/java/br/com/viduink/quatorze_api_agenda/entities/Tarefa.java
@@ -1,4 +1,42 @@
 package br.com.viduink.quatorze_api_agenda.entities;
 
+import br.com.viduink.quatorze_api_agenda.enums.Prioridade;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Entity
+@Table(name = "tarefas")
+@Data
 public class Tarefa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "nome", length = 150, nullable = false)
+    private String nome;
+
+    @Column(name = "datainicio", nullable = false)
+    private LocalDate dataInicio;
+
+    @Column(name = "horainicio", nullable = false)
+    private LocalTime horaInicio;
+
+    @Column(name = "datafim")
+    private LocalDate dataFim;
+
+    @Column(name = "horafim")
+    private LocalTime horaFim;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "prioridade", nullable = false)
+    private Prioridade prioridade;
+
+    private Categoria categoria;
+
+    private Usuario usuario;
 }
