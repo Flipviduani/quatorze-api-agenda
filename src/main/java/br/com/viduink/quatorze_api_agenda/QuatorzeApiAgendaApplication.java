9db19a04510dc7f4a33280b12156fa1978753f9f
Flipviduani <flipviduani@hotmail.com>
2026-08-24 10:07:11 -0300

    Initial commit

---
+++ src/main/java/br/com/viduink/quatorze_api_agenda/QuatorzeApiAgendaApplication.java
@@ -0,0 +1,13 @@
+package br.com.viduink.quatorze_api_agenda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuatorzeApiAgendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuatorzeApiAgendaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/viduink/quatorze_api_agenda/QuatorzeApiAgendaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.viduink.quatorze_api_agenda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuatorzeApiAgendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
