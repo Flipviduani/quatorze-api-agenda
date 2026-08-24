14138698d745845e770f566438fcf908218d00c4
Flipviduani <flipviduani@hotmail.com>
2026-08-24 11:24:51 -0300

    Added "Usuario" Class attributes.

--- src/main/java/br/com/viduink/quatorze_api_agenda/entities/Usuario.java
+++ src/main/java/br/com/viduink/quatorze_api_agenda/entities/Usuario.java
@@ -1,4 +1,16 @@
 package br.com.viduink.quatorze_api_agenda.entities;
 
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
 public class Usuario {
+    private Integer id;
+    private String nome;
+    private String email;
+    private String senha;
+    private LocalDateTime dataHoraCriacao;
+    private List<Tarefa> tarefas;
 }
