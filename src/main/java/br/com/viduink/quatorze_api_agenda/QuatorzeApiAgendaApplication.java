7e119d9110fe661a31c393893ca63f7107603aa1
Flipviduani <flipviduani@hotmail.com>
2026-08-26 11:56:18 -0300

    General code fixes in main.

--- src/main/java/br/com/viduink/quatorze_api_agenda/QuatorzeApiAgendaApplication.java
+++ src/main/java/br/com/viduink/quatorze_api_agenda/QuatorzeApiAgendaApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class QuatorzeApiAgendaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(QuatorzeApiAgendaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(QuatorzeApiAgendaApplication.class, args);
+    }
 
 }
