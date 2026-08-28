168bba20ae372e2f477c72d2dbc0d9f59327d990
Flipviduani <flipviduani@hotmail.com>
2026-08-28 10:57:28 -0300

    Updates User Repository.

--- src/main/java/br/com/viduink/quatorze_api_agenda/repositories/UsuarioRepository.java
+++ src/main/java/br/com/viduink/quatorze_api_agenda/repositories/UsuarioRepository.java
@@ -2,8 +2,17 @@ package br.com.viduink.quatorze_api_agenda.repositories;
 
 import br.com.viduink.quatorze_api_agenda.entities.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+
+    @Query("""
+            SELECT u FROM Usuario u
+            WHERE u.email = :email
+            """)
+    Usuario findByEmail(@Param("email") String email);
+
 }
