0fbb826bd73b1698691d50e47616a2ab1df8ab12
Flipviduani <flipviduani@hotmail.com>
2026-08-28 13:12:22 -0300

    Adds New Access Denied Exception

---
+++ src/main/java/br/com/viduink/quatorze_api_agenda/exceptions/AcessoNegadoException.java
@@ -0,0 +1,7 @@
+package br.com.viduink.quatorze_api_agenda.exceptions;
+
+public class AcessoNegadoException extends RuntimeException {
+    public AcessoNegadoException(String message) {
+        super(message);
+    }
+}
