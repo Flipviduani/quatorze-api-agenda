2e63887b6e95be8b30c2929959edde80ff02da87
Flipviduani <flipviduani@hotmail.com>
2026-08-28 13:13:29 -0300

    Updates User Service

--- src/main/java/br/com/viduink/quatorze_api_agenda/services/UsuarioService.java
+++ src/main/java/br/com/viduink/quatorze_api_agenda/services/UsuarioService.java
@@ -1,22 +1,52 @@
 package br.com.viduink.quatorze_api_agenda.services;
 
+import br.com.viduink.quatorze_api_agenda.dtos.AutenticarUsuarioRequest;
+import br.com.viduink.quatorze_api_agenda.dtos.AutenticarUsuarioResponse;
 import br.com.viduink.quatorze_api_agenda.dtos.CriarUsuarioRequest;
 import br.com.viduink.quatorze_api_agenda.dtos.CriarUsuarioResponse;
 import br.com.viduink.quatorze_api_agenda.entities.Usuario;
+import br.com.viduink.quatorze_api_agenda.exceptions.AcessoNegadoException;
 import br.com.viduink.quatorze_api_agenda.exceptions.EmailJaCadastradoException;
 import br.com.viduink.quatorze_api_agenda.repositories.UsuarioRepository;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.time.LocalDateTime;
+import java.util.Date;
 
 @Service
 public class UsuarioService {
 
     @Autowired
     private UsuarioRepository usuarioRepository;
+
+    public AutenticarUsuarioResponse autenticarUsuario(AutenticarUsuarioRequest request) throws Exception{
+        var usuario = usuarioRepository.findByEmail(request.email());
+
+        if (usuario == null){
+            throw new AcessoNegadoException("Acesso negado. Usuário não encontrado.");
+        }
+
+        var senhaCriptografada = criptografarSenha(request.senha());
+
+        if( ! usuario.getSenha().equals(senhaCriptografada)){
+            throw new AcessoNegadoException("Acesso negado. Credenciais inválidas.");
+        }
+
+        return new AutenticarUsuarioResponse(
+                "Usuário autenticado com sucesso!",
+                LocalDateTime.now(),
+                usuario.getId(),
+                usuario.getNome(),
+                usuario.getEmail(),
+                gerarToken(usuario.getEmail())
+        );
+
+    }
 
     public CriarUsuarioResponse criarUsuario(CriarUsuarioRequest request) throws Exception {
 
@@ -40,6 +70,16 @@ public class UsuarioService {
                 usuario.getNome(),
                 usuario.getEmail()
         );
+    }
+
+    private String gerarToken(String email) throws Exception{
+        var chaveAssinatura = "4e3321d3-bad5-4d0a-a1f0-9e2286d9094f";
+
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(new Date())
+                .signWith(SignatureAlgorithm.HS256, chaveAssinatura)
+                .compact();
     }
 
     private String criptografarSenha(String senha) throws Exception {
