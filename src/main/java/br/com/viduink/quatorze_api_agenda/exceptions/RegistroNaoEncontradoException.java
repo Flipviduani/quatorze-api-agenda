58b2df5d2f49a883170bf486a47f52308e79dee8
flipviduani <flipviduani@hotmail.com>
2026-09-04 09:48:36 -0300

    Added Exception

---
+++ src/main/java/br/com/viduink/quatorze_api_agenda/exceptions/RegistroNaoEncontradoException.java
@@ -0,0 +1,7 @@
+package br.com.viduink.quatorze_api_agenda.exceptions;
+
+public class RegistroNaoEncontradoException extends RuntimeException {
+    public RegistroNaoEncontradoException(String message) {
+        super(message);
+    }
+}
