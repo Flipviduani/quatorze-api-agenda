3e5a9a0e473fe385797e3c949da2a9947d199265
flipviduani <flipviduani@hotmail.com>
2026-09-04 10:20:20 -0300

    Updates Categoria and Tarefa Service

--- src/main/java/br/com/viduink/quatorze_api_agenda/services/CategoriaService.java
+++ src/main/java/br/com/viduink/quatorze_api_agenda/services/CategoriaService.java
@@ -1,7 +1,37 @@
 package br.com.viduink.quatorze_api_agenda.services;
 
+import br.com.viduink.quatorze_api_agenda.dtos.ConsultarCategoriaResponse;
+import br.com.viduink.quatorze_api_agenda.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CategoriaService {
+
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+
+    /*
+        Método para consultar todas as categorias cadastradas
+        na tabela do banco de dados e retorna-la usando o DTO
+     */
+    public List<ConsultarCategoriaResponse> consultarCategorias() throws Exception {
+
+        //Consultar todas as categorias no banco de dados
+        var categorias = categoriaRepository.findAll();
+
+        //Copiar os dados da lista de 'Categoria' para uma lista de 'ConsultarCategoriaResponse'
+        var response = categorias.stream()
+                .map(categoria -> {
+                    var dto = new ConsultarCategoriaResponse(
+                            categoria.getId(),
+                            categoria.getNome()
+                    );
+                    return dto;
+                }).toList();
+
+        return response;
+    }
 }
