cb6cffb24588ef5d2734a1f53de274133bf226c9
flipviduani <flipviduani@hotmail.com>
2026-09-04 09:50:24 -0300

    Updates DTOs

--- src/main/java/br/com/viduink/quatorze_api_agenda/dtos/CriarUsuarioRequest.java
+++ src/main/java/br/com/viduink/quatorze_api_agenda/dtos/CriarUsuarioRequest.java
@@ -7,20 +7,18 @@ import jakarta.validation.constraints.Size;
 
 public record CriarUsuarioRequest(
 
-        @Size(min = 1, max = 100, message  = "O nome do usuário deve ter pelo menos 8 caracteres.")
+        @Size(min = 8, message = "O nome do usuário deve ter pelo menos 8 caracteres.")
         @NotEmpty(message = "O nome do usuário é obrigatório.")
         String nome,
 
-        @Email(message = "Informe um endereço de e-mail válido")
-        @NotEmpty(message = "O e-mail do usuário é obrigatório.")
+        @Email(message = "Informe um endereço de email válido.")
+        @NotEmpty(message = "O email do usuário é obrigatório.")
         String email,
 
         @Pattern(
                 regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$",
-                message = "A senha deve ter pelo menos uma letra minúscula, maiúscula, número e caractere especial, " +
-                "assim como no mínimo 8 caractereres."
+                message = "A senha deve ter pelo menos 1 letra minúscula, 1 letra maiúscula, 1 número, 1 caractere especial e no mínimo 8 caracteres."
         )
-
         @NotEmpty(message = "A senha do usuário é obrigatória.")
         String senha
 ) {
